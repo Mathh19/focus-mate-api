789c6242983f8da82d668074b32fac6e213152e0
Matheus <mf635097@gmail.com>
2023-09-08 14:27:01 -0300

    feat: ensure GetUsersMongoRepository gets users from mongo

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,12 +1,11 @@
 import { IGetUsersRepository } from "../../controllers/get-users/protocols";
+import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/user";
 
-export class MongoGetUsersRepository implements  IGetUsersRepository {
+export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
-      return [{
-        username: 'Math',
-        email: 'math@gmail.com',
-        password: '******'
-      }]
+    const users = await MongoClient.db.collection<Omit<User, 'id'>>('users').find({}).toArray();
+
+    return users.map(({ _id, ...rest }) => ({ ...rest, id: _id.toHexString() }));
   }
 }
