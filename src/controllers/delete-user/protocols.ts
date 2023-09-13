1ea69fa1ec0d77ae60aaff13e0791bc7bb16ba04
Matheus <mf635097@gmail.com>
2023-09-13 12:24:10 -0300

    chore: remove users data

--- src/controllers/delete-user/protocols.ts
+++
@@ -1,4 +0,0 @@
-import { User } from "../../models/user";
-export interface IDeleteUserReposistory {
-  deleteUser(id: string): Promise<User>;
-}
--- src/controllers/update-user/protocols.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "../../models/user";
-export interface UpdateUserParams {
-  username?: string;
-  password?: string;
-}
-
-export interface IUpdateUserRepository {
-  update(id: string, params: UpdateUserParams): Promise<User>
-}
