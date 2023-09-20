1229d25224dc476de2429ec7de1aba9e2718a91b
Matheus <mf635097@gmail.com>
2023-09-20 16:45:30 -0300

    fix: removing unused packages

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
 import express from 'express';
 import { config } from 'dotenv';
 import cors from 'cors';
-import http from 'http';
-import compression from 'compression';
-import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser';
 import router from './router';
 import { connectToDB } from './database/connectToDb'
 
@@ -13,16 +9,14 @@ const app = express();
 app.use(cors({
   credentials: true,
 }));
-app.use(compression());
-app.use(cookieParser());
-app.use(bodyParser.json());
 
-const server = http.createServer(app);
+app.use(express.json());
+
 
 const port = process.env.PORT || 8000;
 
-server.listen(port, () => console.log(`Server running on port ${port}`));
+app.use(router());
 
 connectToDB();
 
-app.use('/', router());
+app.listen(port, () => console.log(`Server running on port ${port}`));
