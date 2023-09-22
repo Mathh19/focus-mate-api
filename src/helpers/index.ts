9300547ae74c7fbc48dfcf43ba316c12329342c9
Matheus <mf635097@gmail.com>
2023-09-22 18:32:39 -0300

    initial project

--- src/helpers/index.ts
+++
@@ -1,5 +0,0 @@
-import bcrypt from 'bcrypt';
-
-export const authenticationBcrypt = async (password: string) => {
-  return await bcrypt.hash(password, 10)
-}
--- src/router/authentication.ts
+++
@@ -1,8 +0,0 @@
-import { UserController } from '../controllers/UserController';
-import express from 'express';
-
-export default (router: express.Router) => {
-  const user = new UserController();
-  router.post('/auth/register', user.create);
-  router.post('/auth/login', user.login);
-}
