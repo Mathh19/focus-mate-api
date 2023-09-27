ff9c54c35d9f8b0c33de2cc3414e3101f0ba5e27
Matheus <mf635097@gmail.com>
2023-09-27 16:17:58 -0300

    feat: add conncetion with db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI, { dbName: 'focus-mate' }),
+    UserModule,
+    AuthModule,
+  ],
 })
 export class AppModule { }
