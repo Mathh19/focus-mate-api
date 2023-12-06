0e174a22d230329dde87febd26a867b2ab292027
Matheus <mf635097@gmail.com>
2023-12-06 15:22:05 -0300

    fix: fix remove id from Interceptor

--- src/task/remove-id.interceptor.ts
+++ src/task/remove-id.interceptor.ts
@@ -6,6 +6,7 @@ export class RemoveIdInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const request = context.switchToHttp().getRequest();
     delete request.body.id;
+    delete request.body._id;
 
     return next.handle();
   }
