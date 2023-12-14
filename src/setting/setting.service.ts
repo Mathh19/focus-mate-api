38ec135e41ca976b0a743334b9f422c4dc545cba
Matheus <mf635097@gmail.com>
2023-12-14 14:03:29 -0300

    fix: fix value volume not updated

--- src/setting/setting.service.ts
+++ src/setting/setting.service.ts
@@ -30,7 +30,7 @@ export class SettingService {
       notification,
       routineMode,
       theme,
-      volume: volume ? volume[0] : volume
+      volume
     });
 
     if (pomodoroTime < 0 || shortRestTime < 0 || longRestTime < 0 || cycles < 0) {
