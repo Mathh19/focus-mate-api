146cafb6332875b1a9d376eea3b9a7c3d5312b02
Matheus <mf635097@gmail.com>
2024-01-16 18:05:39 -0300

    feat: add MulterModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,15 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { TaskModule } from './task/task.module';
 import { SettingModule } from './setting/setting.module';
+import { MulterModule } from '@nestjs/platform-express';
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true
+    }),
+    MulterModule.register({
+      dest: './upload',
     }),
     MongooseModule.forRoot(process.env.MONGODB_URI, { dbName: process.env.MONGODB_NAME }),
     UserModule,
